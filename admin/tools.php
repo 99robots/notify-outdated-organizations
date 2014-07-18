0d5fb6fa621bab2e0f1310c3d59dbc401347f811
Kyle <kjbenk@gmail.com>
2014-07-18 12:36:59 -0400

    Added Features
    See all Outdated Organizations

--- admin/tools.php
+++ admin/tools.php
@@ -1,3 +1,105 @@
 <h1><?php _e('Notify Outdated Organizations', self::$text_domain); ?></h1>
 
-<input type="button" class="button" id="<?php self::$prefix; ?>notify" name="<?php self::$prefix; ?>notify" value="<?php _e('Notify all outdated Organizations'); ?>"/>
+<!-- Notify Button -->
+
+<p>
+	<a href="<?php echo "tools.php?page=" . self::$tools_page . '&notify=true'; ?>"><input type="button" class="button" id="<?php self::$prefix; ?>notify" name="<?php self::$prefix; ?>notify" value="<?php _e('Notify all outdated Organizations', self::$text_domain); ?>" /></a><br/>
+	<em><?php echo sprintf(__('Last Notifed on %s'), $updated_last); ?></em>
+</p>
+
+<!-- Category Filter -->
+
+<!--
+<p>
+	<select id="<?php echo self::$prefix; ?>category" name="<?php echo self::$prefix; ?>category">
+		<?php
+		$categories = get_terms('org_category');
+
+		foreach ($categories as $category) {?>
+			<option value="<?php echo $category->name; ?>"><?php echo $category->name; ?></option>
+		<?php } ?>
+	</select>
+</p>
+-->
+
+<div class="wrap">
+
+	<h3><?php _e('Outdated Organizations', self::$text_domain); ?></h3>
+
+	<?php
+	$organizations = get_posts(array(
+		'post_type' 		=> 'organization',
+		'posts_per_page' 	=> -1,
+		'post_status' 		=> 'publish',
+		'date_query' => array(
+			array(
+				'column' 	=> 'post_modified_gmt', //post_modified_gmt
+				'before'  	=> '1 year ago',
+			),
+		),
+	));
+	?>
+	<div class="tablenav top">
+		<div class="tablenav-pages one-page">
+			<span class="displaying-num"><?php echo sprintf(__('%s Items'), count($organizations)); ?></span>
+		</div>
+	</div>
+
+	<table class="wp-list-table widefat fixed posts">
+		<thead>
+			<th><?php _e('ID', self::$text_domain); ?></th>
+			<th><?php _e('Title', self::$text_domain); ?></th>
+			<th><?php _e('Email', self::$text_domain); ?></th>
+			<th><?php _e('Category', self::$text_domain); ?></th>
+			<th><?php _e('Last Updated', self::$text_domain); ?></th>
+		</thead>
+		<tfoot>
+			<th><?php _e('ID', self::$text_domain); ?></th>
+			<th><?php _e('Title', self::$text_domain); ?></th>
+			<th><?php _e('Email', self::$text_domain); ?></th>
+			<th><?php _e('Category', self::$text_domain); ?></th>
+			<th><?php _e('Last Updated', self::$text_domain); ?></th>
+		</tfoot>
+
+		<tbody id="the-list">
+		<?php foreach ($organizations as $organization) { ?>
+			<tr>
+
+				<td>
+					<span><?php _e($organization->ID, self::$text_domain); ?></span>
+				</td>
+
+				<td>
+					<a href="<?php echo get_permalink($organization->ID); ?>"><?php _e($organization->post_title, self::$text_domain); ?></a>
+					<div class="row-actions">
+						<span class="edit"><a href="post.php?post=<?php _e($organization->ID, self::$text_domain); ?>&action=edit"><?php _e("Edit", self::$text_domain); ?></a></span> |
+						<span class="view"><a href="<?php echo get_permalink($organization->ID); ?>"><?php _e("View", self::$text_domain); ?></a></span>
+					</div>
+				</td>
+
+				<td>
+					<span><?php _e(get_post_meta($organization->ID, '_OrganizerEmail', true ), self::$text_domain); ?></span>
+				</td>
+
+				<td>
+					<?php foreach (get_the_terms($organization->ID, 'org_category') as $category) { ?>
+						<div><?php _e($category->name, self::$text_domain); ?></div>
+					<?php } ?>
+				</td>
+
+				<td>
+					<abbr><?php _e(date_format(date_create($organization->post_modified_gmt), "Y/m/d"), self::$text_domain); ?></abbr><br/>
+					<span><?php _e("Modified", self::$text_domain); ?></span>
+				</td>
+
+			</tr>
+		<?php } ?>
+		</tbody>
+	</table>
+
+	<div class="tablenav bottom">
+		<div class="tablenav-pages one-page">
+			<span class="displaying-num"><?php echo sprintf(__('%s Items'), count($organizations)); ?></span>
+		</div>
+	</div>
+</div>
---
+++ notify-outdated-organizations.php
@@ -0,0 +1,218 @@
+<?php
+/*
+Plugin Name: Notify Outdated Organizations
+plugin URI:
+Description: Notifies the outdated organizations (over 1 year of no updates).
+version: 1.0
+Author: Stratiq
+Author URI: http://stratiq.com
+License: GPL2
+*/
+
+/**
+ * Global Definitions
+ */
+
+/* Plugin Name */
+
+if (!defined('NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_NAME'))
+    define('NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_NAME', trim(dirname(plugin_basename(__FILE__)), '/'));
+
+/* Plugin directory */
+
+if (!defined('NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_DIR'))
+    define('NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_NAME);
+
+/* Plugin url */
+
+if (!defined('NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_URL'))
+    define('NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_URL', WP_PLUGIN_URL . '/' . NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_NAME);
+
+/* Plugin verison */
+
+if (!defined('NOTIFY_OUTDATED_ORGANIZATIONS_VERSION_NUM'))
+    define('NOTIFY_OUTDATED_ORGANIZATIONS_VERSION_NUM', '1.0.0');
+
+
+/**
+ * Activatation / Deactivation
+ */
+
+register_activation_hook( __FILE__, array('NotifyOutdatedOrganizations', 'register_activation'));
+
+/**
+ * Hooks / Filter
+ */
+
+add_action('init', array('NotifyOutdatedOrganizations', 'load_textdomain'));
+add_action('admin_menu', array('NotifyOutdatedOrganizations', 'menu_page'));
+
+$plugin = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin", array('NotifyOutdatedOrganizations', 'plugin_links'));
+
+/**
+ *  NotifyOutdatedOrganizations main class
+ *
+ * @since 1.0.0
+ * @using Wordpress 3.8
+ */
+
+class NotifyOutdatedOrganizations {
+
+	/* Properties */
+
+	private static $text_domain = 'notify-outdated-organizations';
+
+	private static $prefix = 'notify_outdated_organizations_';
+
+	private static $tools_page = 'notify-outdated-organizations-admin-tools';
+
+	private static $default = array(
+		'updated_last'	=> '',
+	);
+
+	/**
+	 * Load the text domain
+	 *
+	 * @since 1.0.0
+	 */
+	static function load_textdomain() {
+		load_plugin_textdomain(self::$text_domain, false, NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_DIR . '/languages');
+	}
+
+	/**
+	 * Hooks to 'register_activation_hook'
+	 *
+	 * @since 1.0.0
+	 */
+	static function register_activation() {
+
+		/* Check if multisite, if so then save as site option */
+
+		if (is_multisite()) {
+			add_site_option(self::$prefix . 'version', NOTIFY_OUTDATED_ORGANIZATIONS_VERSION_NUM);
+		} else {
+			add_option(self::$prefix . 'version', NOTIFY_OUTDATED_ORGANIZATIONS_VERSION_NUM);
+		}
+	}
+
+	/**
+	 * Hooks to 'plugin_action_links_' filter
+	 *
+	 * @since 1.0.0
+	 */
+	static function plugin_links($links) {
+		$tools_link = '<a href="tools.php?page=' . self::$tools_page . '">Tools</a>';
+		array_unshift($links, $tools_link);
+		return $links;
+	}
+
+	/**
+	 * Hooks to 'admin_menu'
+	 *
+	 * @since 1.0.0
+	 */
+	static function menu_page() {
+
+	    /* Cast the first sub menu to the top menu */
+
+	    $tools_page_load = add_submenu_page(
+	    	'tools.php', 										// parent slug
+	    	__('Notify Outdated Organizations', self::$text_domain), 						// Page title
+	    	__('Notify Outdated Organizations', self::$text_domain), 						// Menu name
+	    	'manage_options', 											// Capabilities
+	    	self::$tools_page, 										// slug
+	    	array('NotifyOutdatedOrganizations', 'tools_page')	// Callback function
+	    );
+	    add_action("admin_print_scripts-$tools_page_load", array('NotifyOutdatedOrganizations', 'include_admin_scripts'));
+	}
+
+	/**
+	 * Hooks to 'admin_print_scripts-$page'
+	 *
+	 * @since 1.0.0
+	 */
+	static function include_admin_scripts() {
+
+		/* CSS */
+
+		wp_register_style(self::$prefix . 'tools_css', NOTIFY_OUTDATED_ORGANIZATIONS_PLUGIN_URL . '/css/tools.css');
+		wp_enqueue_style(self::$prefix . 'tools_css');
+	}
+
+	/**
+	 * Displays the HTML for the 'notify-outdated-organizations-admin-menu-settings' admin page
+	 *
+	 * @since 1.0.0
+	 */
+	static function tools_page() {
+
+		$settings = get_option(self::$prefix . 'settings');
+
+		/* Default values */
+
+		if ($settings === false) {
+			$settings = self::$default;
+		}
+
+		$updated_last = isset($settings['updated_last']) ? $settings['updated_last'] : '';
+
+		if (isset($_GET['notify']) && $_GET['notify'] == 'true') {
+
+			$settings['updated_last'] = date("Y/m/d h:m:s T");
+
+			update_option(self::$prefix . 'settings', $settings);
+
+			self::email_organizations();
+
+			?>
+			<script type="text/javascript">
+				window.location = "<?php echo $_SERVER['PHP_SELF']?>?page=<?php echo self::$tools_page; ?>";
+			</script>
+			<?php
+		} else {
+			;
+		}
+
+		require('admin/tools.php');
+	}
+
+	/**
+	 * Emails all organizations that have no updated their info in over a year
+	 *
+	 * @access public
+	 * @static
+	 * @return void
+	 */
+	static function email_organizations() {
+		$organizations = get_posts(array(
+			'post_type' 		=> 'organization',
+			'posts_per_page' 	=> -1,
+			'post_status' 		=> 'publish',
+			'date_query' => array(
+				array(
+					'column' 	=> 'post_modified_gmt',
+					'before'  	=> '1 year ago',
+				),
+			),
+		));
+
+		foreach ($organizations as $organization) {
+
+			$email = get_post_meta($organization->ID, '_OrganizerEmail', true);
+
+			$url = get_permalink($organization->ID);
+
+			$subject = "Update your Organization: $organization->post_title";
+
+			$message = "Please update your Organization: $organization->post_title";
+			$message .= "\r\n\r\n";
+	    	$message .= "View it: $url";
+
+			if (isset($email) && $email != '') {
+				wp_mail($email, $subject, $message);
+			}
+		}
+	}
+}
+?>
